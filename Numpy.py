62453dd08893667a6e073f1b855b8de92fea1a51
Chaitanya <Chaitanyadwarapudi123@gmail.com>
2023-06-13 17:22:00 +0530

    added new elements

--- Numpy.py
+++ Numpy.py
@@ -193,3 +193,62 @@ y = np.array([[1,2,3,4],[1,2,3,4]])
 
 result = x + y
 print("Result: ",result)
+
+# Arithmetic Functions
+
+# np.min(x)
+
+var = np.array([1,2,3,4])
+
+print(np.min(var))
+print(np.max(var))
+print(np.argmin(var))
+print(np.sqrt(var))
+print(np.sin(var))
+print(np.cos(var))
+print(np.cumsum(var))
+
+# Shape and Reshape of the Array
+
+var = np.array([[1,2,3,4],[1,2,3,4]])
+
+print(var)
+print()
+print(var.shape)
+
+var = np.array([1,2,3,4],ndmin=4)
+
+print(var)
+print(var.ndim)
+print()
+print(var.shape)
+
+var = np.array([1,2,3,4,5,6])
+print(var)
+
+print()
+
+x = var.reshape(3,2)
+print(x)
+
+# Indexing and Slicing
+
+# Indexing
+
+var = np.array([1, 2, 3, 4])
+
+print(var[1])
+print(var[-1])
+
+
+var = np.array([[1,2,3,4],[1,2,3,4]])
+print(var[0,1])
+
+var = np.array([[[1,2,3,4],[1,2,3,4],[1,2,3,4]]])
+print(var[0,0,1])
+
+# slicing
+
+var = np.array([1,2,3,4])
+print(var[1:])
+print(var[1:3])
